d431b66791002261d03490e421b4275e82ce3672
“yuancf1024” <3455911441@qq.com>
2022-04-26 21:43:36 +0800

    将上述新增的功能集成在主流程(geecache.go)中

--- GeeCache/day5-multi-nodes/geecache/geecache.go
+++ GeeCache/day5-multi-nodes/geecache/geecache.go
@@ -12,6 +12,7 @@ type Group struct {
 	name string // 每个 Group 拥有一个唯一的名称 name
 	getter Getter // 缓存未命中时获取源数据的回调(callback)
 	mainCache cache // 一开始实现的并发缓存
+	peers PeerPicker
 }
 
 // Getter为一个键加载数据
@@ -73,9 +74,37 @@ func (g *Group) Get(key string) (ByteView, error) {
 	return g.load(key)
 }
 
+// RegisterPeers registers a PeerPicker for choosing remote peer
+// 新增 RegisterPeers() 方法，将 实现了 PeerPicker 接口的 HTTPPool 注入到 Group 中。
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
 // load 调用 getLocally（分布式场景下会调用 getFromPeer 从其他节点获取）
 func (g *Group) load(key string) (value ByteView, err error) {
-	return g.getLocally(key)
+	if g.peers != nil {
+		if peer, ok := g.peers.PickerPeer(key); ok { // 修改 load 方法，使用 PickPeer() 方法选择节点
+			if value, err = g.getFromPeer(peer, key); err == nil { // 若非本机节点，则调用 getFromPeer() 从远程获取
+				return value, nil
+			}
+			log.Println("[GeeCache] Failed to get from peer", err)
+		}
+	}
+
+	return g.getLocally(key) // 若是本机节点或失败，则回退到 getLocally()。
+}
+
+// 新增 getFromPeer() 方法，使用实现了 PeerGetter 接口的 httpGetter 
+// 从访问远程节点，获取缓存值
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
 }
 
 // getLocally 调用用户回调函数 g.getter.Get() 获取源数据，
